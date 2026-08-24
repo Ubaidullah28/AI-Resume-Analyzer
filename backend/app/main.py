b8aa4f4b91cc8bf1f5e23031eb1170bb16ff6022
Ubaidullah28 <bscs22087@itu.edu.pk>
2026-08-24 15:58:29 +0500

    Prepare backend for production

--- backend/app/main.py
+++ backend/app/main.py
@@ -31,20 +31,46 @@ app = FastAPI(
 # CORS
 # -------------------------
 
+# app.add_middleware(
+#     CORSMiddleware,
+
+#     allow_origins=[
+#         "http://localhost:5173",
+#         "http://127.0.0.1:5173"
+#     ],
+
+#     allow_credentials=True,
+
+#     allow_methods=["*"],
+
+#     allow_headers=["*"]
+# )
+
+
+
+import os
+
+FRONTEND_URL = os.getenv("FRONTEND_URL")
+
+ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+if FRONTEND_URL:
+    ALLOWED_ORIGINS.append(FRONTEND_URL)
+
 app.add_middleware(
     CORSMiddleware,
-
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
-    ],
-
+    allow_origins=ALLOWED_ORIGINS,
     allow_credentials=True,
-
     allow_methods=["*"],
-
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
+
+
+
+
 
 
 # -------------------------
